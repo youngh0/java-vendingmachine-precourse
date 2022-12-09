69a261a040ee27ff0fe9e063b255ae01afd279a9
Youngho <cyh6099@gmail.com>
2022-12-09 17:11:30 +0900

    test : 상품입력 유효성 검사에 테스트 케이스 추가

--- src/test/java/vendingmachine/domain/ProductsInfoTest.java
+++ src/test/java/vendingmachine/domain/ProductsInfoTest.java
@@ -7,7 +7,8 @@ import org.junit.jupiter.params.provider.ValueSource;
 class ProductsInfoTest {
 
     @ParameterizedTest
-    @ValueSource(strings = {"[상품명,10,1]", "[상품명,100,0]", "[상품명,10,1}", "{상품명,10,1]", "[상품명,10,1.1]"})
+    @ValueSource(strings = {"[상품명,10,1]", "[상품명,100,0]", "[상품명,10,1}", "{상품명,10,1]", "[상품명,10,1.1]", "",
+            "[상품명,10,1];[상품명,1000,1]"})
     void addProductTest(String productInfo) {
         ProductsInfo productsInfo = new ProductsInfo(new ProductPrice(), new ProductQuantity());
         Assertions.assertThrows(IllegalArgumentException.class,
