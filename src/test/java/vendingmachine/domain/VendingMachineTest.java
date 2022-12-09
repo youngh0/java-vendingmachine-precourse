1c81850a3f75b0d12a2d50c8b7f2217c14262a63
Youngho <cyh6099@gmail.com>
2022-12-09 15:42:30 +0900

    test : 자판기 보유 금액에 따른 전체 동전 개수 초기화 테스트

---
+++ src/test/java/vendingmachine/domain/VendingMachineTest.java
@@ -0,0 +1,32 @@
+package vendingmachine.domain;
+
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
+import vendingmachine.Application;
+
+import static camp.nextstep.edu.missionutils.test.Assertions.assertRandomNumberInListTest;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class VendingMachineTest extends NsTest {
+
+    @Test
+    void initChangeCoinCount() {
+        assertRandomNumberInListTest(
+                () -> {
+                    Changes changes = new Changes();
+                    VendingMachine vendingMachine = new VendingMachine(changes);
+                    vendingMachine.initChangeCoinCount(410);
+                    run();
+                    assertThat(changes.getChangeCount(Coin.COIN_100)).isEqualTo(4);
+                    assertThat(changes.getChangeCount(Coin.COIN_50)).isEqualTo(0);
+                    assertThat(changes.getChangeCount(Coin.COIN_10)).isEqualTo(1);
+                },
+                100, 100, 100, 100, 50,10
+        );
+    }
+
+    @Override
+    protected void runMain() {
+        Application.main(new String[]{});
+    }
+}
