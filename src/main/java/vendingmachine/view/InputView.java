f863469807403dd196d9861904d880238542eaf9
Youngho <cyh6099@gmail.com>
2022-12-09 21:24:55 +0900

    feat : 예외 발생 시 해당 지점부터 다시 시작하게 구현

--- src/main/java/vendingmachine/view/InputView.java
+++ src/main/java/vendingmachine/view/InputView.java
@@ -4,26 +4,53 @@ import camp.nextstep.edu.missionutils.Console;
 import vendingmachine.domain.*;
 
 public class InputView {
+    private final String errorMessage = "[ERROR] ";
     public VendingMachineMoney readVendingMachineMoney() {
-        System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
-        return new VendingMachineMoney(Console.readLine());
+        while (true) {
+            try {
+                System.out.println("자판기가 보유하고 있는 금액을 입력해 주세요.");
+                return new VendingMachineMoney(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(errorMessage + e.getMessage());
+            }
+        }
+
     }
 
     public ProductsInfo readProducts() {
-        System.out.println("상품명과 가격, 수량을 입력해 주세요.");
-        String products = Console.readLine();
-        ProductsInfo productsInfo = new ProductsInfo(new ProductPrice(), new ProductQuantity());
-        productsInfo.initProductInfo(products);
-        return productsInfo;
+        while (true) {
+            try {
+                System.out.println("상품명과 가격, 수량을 입력해 주세요.");
+                String products = Console.readLine();
+                ProductsInfo productsInfo = new ProductsInfo(new ProductPrice(), new ProductQuantity());
+                productsInfo.initProductInfo(products);
+                return productsInfo;
+            } catch (IllegalArgumentException e) {
+                System.out.println(errorMessage + e.getMessage());
+            }
+        }
     }
 
     public PaymentMoney readMoney() {
-        System.out.println("투입 금액을 입력해 주세요.");
-        return new PaymentMoney(Console.readLine());
+        while (true) {
+            try {
+                System.out.println("투입 금액을 입력해 주세요.");
+                return new PaymentMoney(Console.readLine());
+            } catch (IllegalArgumentException e) {
+                System.out.println(errorMessage + e.getMessage());
+            }
+        }
+
     }
 
     public String readProductName() {
-        System.out.println("구매할 상품명을 입력해 주세요.");
-        return Console.readLine();
+        while (true) {
+            try {
+                System.out.println("구매할 상품명을 입력해 주세요.");
+                return Console.readLine();
+            } catch (IllegalArgumentException e) {
+                System.out.println(errorMessage + e.getMessage());
+            }
+        }
     }
 }
