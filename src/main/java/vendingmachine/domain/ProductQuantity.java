9acedfc2306e606d018fa1e574b85311dee46de0
Youngho <cyh6099@gmail.com>
2022-12-09 17:22:18 +0900

    refactor : 상품 수량 정보 DTO로 저장하게 수정

--- src/main/java/vendingmachine/domain/ProductQuantity.java
+++ src/main/java/vendingmachine/domain/ProductQuantity.java
@@ -1,16 +1,18 @@
 package vendingmachine.domain;
 
+import vendingmachine.dto.ProductQuantityDTO;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProductQuantity {
-    private final Map<String, Integer> productQuantityInfo;
+    private final Map<String, ProductQuantityDTO> productQuantityInfo;
 
     public ProductQuantity() {
         productQuantityInfo = new HashMap<>();
     }
 
-    public void addProductQuantityInfo(String productName, int quantity) {
+    public void addProductQuantityInfo(String productName, ProductQuantityDTO quantity) {
         productQuantityInfo.put(productName, quantity);
     }
 }
