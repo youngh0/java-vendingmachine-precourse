e5e964087246c817814983bfaeffa89e2be09a6f
Youngho <cyh6099@gmail.com>
2022-12-09 17:02:01 +0900

    test : 각각의 상품 입력 형태에 대한 유효성 테스트 진행

---
+++ src/test/java/vendingmachine/domain/ProductsInfoTest.java
@@ -0,0 +1,16 @@
+package vendingmachine.domain;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class ProductsInfoTest {
+
+    @ParameterizedTest
+    @ValueSource(strings = {"[상품명,10,1]", "[상품명,100,0]", "[상품명,10,1}", "{상품명,10,1]", "[상품명,10,1.1]"})
+    void addProductTest(String productInfo) {
+        ProductsInfo productsInfo = new ProductsInfo(new ProductPrice(), new ProductQuantity());
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> productsInfo.addProduct(productInfo));
+    }
+}
