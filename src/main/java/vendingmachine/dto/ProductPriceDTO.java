a1f77056c26aee5c3469ffe86e81e9d7f6df65d0
Youngho <cyh6099@gmail.com>
2022-12-09 16:46:00 +0900

    feat : 상품 가격을 나타내는 dto생성

---
+++ src/main/java/vendingmachine/dto/ProductPriceDTO.java
@@ -0,0 +1,30 @@
+package vendingmachine.dto;
+
+public class ProductPriceDTO {
+    private final int price;
+
+    public ProductPriceDTO(String price) {
+        validate(price);
+        this.price = Integer.parseInt(price);
+    }
+
+    private void validate(String price) {
+        isNumber(price);
+        validatePriceShape(price);
+    }
+
+    private void isNumber(String price) {
+        String numberRegex = "^[1-9][0-9]*$";
+        if (price.matches(numberRegex)) {
+            return;
+        }
+        throw new IllegalArgumentException("가격은 숫자만");
+    }
+
+    private void validatePriceShape(String price) {
+        if (Integer.parseInt(price) >= 100 && price.endsWith("0")) {
+            return;
+        }
+        throw new IllegalArgumentException("가격은 100원 이상 10원 단위의 숫자만");
+    }
+}
