c905f566d6924fc84a897f91f5350417613e135b
Youngho <cyh6099@gmail.com>
2022-12-09 19:22:49 +0900

    test : 입력받은 상품이 존재하는지 판단하는 기능 테스트 작성

--- src/test/java/vendingmachine/domain/ProductsInfoTest.java
+++ src/test/java/vendingmachine/domain/ProductsInfoTest.java
@@ -1,17 +1,31 @@
 package vendingmachine.domain;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
 class ProductsInfoTest {
+    ProductsInfo productsInfo;
+    @BeforeEach
+    void init() {
+        productsInfo = new ProductsInfo(new ProductPrice(), new ProductQuantity());
+    }
 
     @ParameterizedTest
     @ValueSource(strings = {"[상품명,10,1]", "[상품명,100,0]", "[상품명,10,1}", "{상품명,10,1]", "[상품명,10,1.1]", "",
             "[상품명,10,1];[상품명,1000,1]"})
     void addProductTest(String productInfo) {
-        ProductsInfo productsInfo = new ProductsInfo(new ProductPrice(), new ProductQuantity());
         Assertions.assertThrows(IllegalArgumentException.class,
                 () -> productsInfo.initProductInfo(productInfo));
+    }
+
+    @Test
+    void isExistTest() {
+        productsInfo.initProductInfo("[콜라,1000,2];[사이다,1400,1]");
+        productsInfo.show();
+        Assertions.assertThrows(IllegalArgumentException.class,
+                () -> productsInfo.isExistProduct("환타"));
     }
 }
