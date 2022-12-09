6a3268d38b53a4a63b4a26d41c1deca6de853112
Youngho <cyh6099@gmail.com>
2022-12-09 19:53:30 +0900

    feat : 남아있는 수량이 있는지 확인하는 기능 구현

--- src/main/java/vendingmachine/domain/ProductQuantity.java
+++ src/main/java/vendingmachine/domain/ProductQuantity.java
@@ -15,4 +15,13 @@ public class ProductQuantity {
     public void addProductQuantityInfo(String productName, ProductQuantityDTO quantity) {
         productQuantityInfo.put(productName, quantity);
     }
+
+    public boolean checkRemainQuantity() {
+        return productQuantityInfo.values().stream()
+                .anyMatch(quantity -> quantity.getQuantity() > 0);
+    }
+
+    public void buyProduct(String productName, int quantity) {
+        productQuantityInfo.get(productName).buyProduct(quantity);
+    }
 }
