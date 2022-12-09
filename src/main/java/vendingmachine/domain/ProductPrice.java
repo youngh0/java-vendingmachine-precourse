3f531735fdbee8822b1497efb1c953db9caa2cb0
Youngho <cyh6099@gmail.com>
2022-12-09 16:46:56 +0900

    refactor : 상품 가격 저장을 int -> dto로 수정

--- src/main/java/vendingmachine/domain/ProductPrice.java
+++ src/main/java/vendingmachine/domain/ProductPrice.java
@@ -1,16 +1,18 @@
 package vendingmachine.domain;
 
+import vendingmachine.dto.ProductPriceDTO;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class ProductPrice {
-    private final Map<String, Integer> productPriceInfo;
+    private final Map<String, ProductPriceDTO> productPriceInfo;
 
     public ProductPrice() {
         productPriceInfo = new HashMap<>();
     }
 
-    public void addProductPriceInfo(String productName, int price) {
+    public void addProductPriceInfo(String productName, ProductPriceDTO price) {
         productPriceInfo.put(productName, price);
     }
 }
